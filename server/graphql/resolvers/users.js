0c29cad948768250d8ce6cc8bcc228ac2f3467db
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-05-07 01:10:58 -0400

    mutation and query for authorization started

---
+++ server/graphql/resolvers/users.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const {SECRET_KEY} = require ('../../config')
+const User = require('../../models/User')
+
+module.exports = {
+    Mutation: {
+        async register
+        (_, 
+        {
+            registerInput:{
+                username,
+                email,
+                password,
+                confirmPassword
+            }
+        }, 
+        context, 
+        info){
+            //TODO:VALIDATE USERDATA
+            //TODO:MAKE SURE USER DOESNT ALREADY EXIST
+            //TODO:HASH PASSWORD AND CREATE AN AUTH TOKEN
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date()
+            })
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username,
+            }, SECRET_KEY, {expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+    }
+}

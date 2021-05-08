6ff1df9fe09982b17d3982815cdf18c4ff8b7203
Joshua Sepulveda <sepulveda.62@gmail.com>
2021-05-07 23:07:50 -0400

    able to count number of likes and comments

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -4,6 +4,10 @@ const commentsResolvers = require ('./comments')
 const { Subscription } = require('./posts')
 
 module.exports = {
+    Post:{
+        likeCount:(parent) => parent.likes.length,
+        commentCount:(parent)=> parent.comments.length
+    },
     Query:{
         ...postsResolvers.Query
     },
